e0025f23b46de8976ff2a6f666a01a9f5022071c
Manuel Saraiva <manuel.gustavo@gmail.com>
2023-03-30 12:52:10 +0100

    git-open plugin fix

--- ubuntu.sh
+++ ubuntu.sh
@@ -49,6 +49,7 @@ install_oh_my_zsh()
         sh -c "$(wget --no-cache -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --skip-chsh --keep-zshrc"
         git clone --quiet --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
         git clone --quiet --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+        git clone --quiet --depth 1 https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
         git@github.com:manuelgustavo/ubuntu-setup.git
         
         # Change the default shell
